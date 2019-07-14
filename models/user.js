9dcc9d081ebb08adbe560866f4ff53287e12d868
gabogoses <gabrielmargoses@gmail.com>
2019-07-14 22:15:33 +0200

    add bcrypt Password hashing :lock:

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 // const userSchema = new Schema({
@@ -34,6 +35,17 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, lowercase: true },
   password: { type: String, min: 6, max: 1024, required: true }
+});
+
+userSchema.pre('save', async function(next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('user', userSchema);
