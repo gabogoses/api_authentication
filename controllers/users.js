913a5a6125c8574e1f1ffe5042bc53275d63fdb4
gabogoses <gabrielmargoses@gmail.com>
2019-07-14 21:18:14 +0200

    New feature: SignToken function :sparkles:

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,19 @@
+const JWT = require('jsonwebtoken');
+
+const keys = require('../config/keys');
 const User = require('../models/user');
+
+signToken = user => {
+  return JWT.sign(
+    {
+      iss: 'API_Authentication',
+      sub: user.id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1)
+    },
+    keys.JWT.SecretOrKey
+  );
+};
 
 module.exports = {
   signUp: async (req, res, next) => {
@@ -12,7 +27,9 @@ module.exports = {
     const newUser = new User({ username, email, password });
     await newUser.save();
 
-    res.json({ user: 'created' });
+    const token = signToken(newUser);
+
+    res.status(200).json({ token });
   },
 
   signIn: async (req, res, next) => {},
